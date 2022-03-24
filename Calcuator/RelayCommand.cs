adde0e105b7c8dd1b2bbd37d970c6921cdd5e86e
asengin <worksmail2017@gmail.com>
2022-03-24 20:32:55 +0300

    Построение структуры MVVM

---
+++ Calcuator/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Calcuator
+{
+    class RelayCommand : ICommand
+
+    {
+        #region Поля
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+        #endregion
+
+        #region Реализация интерфейса ICommand
+        public event EventHandler CanExecuteChanged //Передача управления CommandManager-у
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public RelayCommand(Action<object> Execute, Func<object, bool> CanExecute = null) //Конструктор, принимающий 2 делегата, соответствующих методам Execute и CanExecute
+        {
+            execute = Execute ?? throw new ArgumentNullException(nameof(Execute)); // присваивается, если не null. усли null, то исключение
+            canExecute = CanExecute;
+        }
+
+        public bool CanExecute(object parameter) => canExecute?.Invoke(parameter) ?? true; //если не null, то Invoke. Если null, то вернет true
+
+        public void Execute(object parameter) => execute(parameter);
+        
+        #endregion
+    }
+}
---
+++ Calcuator/ViewModels/MainWindowsViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcuator.ViewModels
+{
+    class MainWindowsViewModel : INotifyPropertyChanged
+    {
+        #region Событие-уведомление об изменении и метод вызова Invoke
+        public event PropertyChangedEventHandler PropertyChanged; //Реализация интерфейса INotifyPropertyChanged
+
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName)); //Запуск методов, переданных с PropertyChanged
+        }
+        #endregion
+
+        private string displayFormula;
+
+        public string DisplayFormula
+        {
+            get => displayFormula;
+            set
+            {
+                displayFormula = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+    }
+
+}

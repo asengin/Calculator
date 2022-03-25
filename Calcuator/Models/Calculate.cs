8093b1c20a557a150e07ad905f4cb380794384e5
asengin <worksmail2017@gmail.com>
2022-03-25 18:14:47 +0300

    Команды, привязки

---
+++ Calcuator/Models/Calculate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcuator.Models
+{
+    class Calculate
+    {
+        public static double Result(string formula)
+        {
+
+            return 5;
+        }
+    }
+}
--- Calcuator/ViewModels/MainWindowsViewModel.cs
+++ Calcuator/ViewModels/MainWindowsViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Calcuator.ViewModels
 {
@@ -19,19 +20,85 @@ namespace Calcuator.ViewModels
         }
         #endregion
 
-        private string displayFormula;
+        #region Свойство отвечающее за взаимодействие с дисплеем с формулой
+        private string displayExpression;
 
-        public string DisplayFormula
+        public string DisplayExpression
         {
-            get => displayFormula;
+            get => displayExpression;
             set
             {
-                displayFormula = value;
+                displayExpression = value;
                 OnPropertyChanged();
             }
         }
+        #endregion
+
+        #region Свойство, отвечающее за взаимодействие с дисплеем результата
+        private double displayResult;
+
+        public double DisplayResult
+        {
+            get { return displayResult; }
+            set { displayResult = value; OnPropertyChanged(); }
+        }
+        #endregion
+
+        public MainWindowsViewModel() //Конструктор с начальной инициализацией
+        {
+            displayExpression = string.Empty;
+            displayResult = 0;
+
+            PressCalcButton = new RelayCommand(OnPressCalcButtonExecute);
+            PressOperationButton = new RelayCommand(OnPressOperationButon);
+        }
+
+        #region Команда нажатия кнопки цифр
+        public ICommand PressCalcButton { get; }
+        private void OnPressCalcButtonExecute(object parameter)
+        {
+            DisplayExpression += parameter.ToString();
+            DisplayResult = Convert.ToDouble(DisplayResult.ToString() + parameter.ToString()); //Выполнение конкатенации вводимых чисел, для отображения на дисплее ввода
+        }
+        #endregion
+
+        #region Команда нажатия кнопок действий
+        public ICommand PressOperationButton { get; }
+        private void OnPressOperationButon(object parameter)
+        {
+            string strP = parameter.ToString(); //Перевод параметра в строку
+            if (strP == "-" || strP == "+" || strP == "*" || strP == "/" || strP == "^2" || strP == "(" || strP == ")")
+            {
+                DisplayResult = 0;
+                DisplayExpression += strP;
+            }
+            else if (strP == "sqrt")
+            {
+                DisplayResult = 0;
+                DisplayExpression += strP + "(";
+            }
+            else if (strP == "negative")
+            {
+                DisplayResult *= -1;
+                DisplayExpression += DisplayResult.ToString(); //Доработать
+            }
+            else if (strP == "C")
+            {
+                DisplayResult = 0;
+                DisplayExpression.Remove(0);
+            }
+            else if (strP == "CE")
+            {
+                if (DisplayResult != 0 && DisplayResult.ToString().Length != 1) //Доработать
+                {
+                    DisplayResult = Convert.ToDouble(DisplayResult.ToString().Remove(DisplayResult.ToString().Length - 1, 1));
+                    DisplayExpression = DisplayExpression.Remove(DisplayExpression.Length - 1, 1); //Доработать
+                }
+            }
 
 
+        }
+        #endregion
     }
 
 }
